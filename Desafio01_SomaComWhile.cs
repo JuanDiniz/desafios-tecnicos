fb86eef31323a572ec6938a45591dd4332f252d0
Juan Diniz <162530319+JuanDiniz@users.noreply.github.com>
2025-04-13 21:12:26 -0300

    Update Desafio01_SomaComWhile.cs

--- Desafio01_SomaComWhile.cs
+++ Desafio01_SomaComWhile.cs
@@ -1,1 +1,18 @@
+// Desafio 1 - Soma com laço WHILE
+using System;
 
+class Desafio1
+{
+    static void Main()
+    {
+        int INDICE = 13, SOMA = 0, K = 0;
+
+        while (K < INDICE)
+        {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+
+        Console.WriteLine("Valor final da variável SOMA: " + SOMA);
+    }
+}
